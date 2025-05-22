ddd0b4e00071416fd3e1adb7c9073fa55ec8546d
santoshdannana <santoshdannana1@gmail.com>
2025-05-22 03:31:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["https://compare-tools.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
